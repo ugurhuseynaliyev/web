65be50e15e75eeb605aa50f0ddb8236ba4ea0505
Ugur Huseynaliyev <huseyneliyevugur06@gmail.com>
2026-06-22 21:22:55 +0400

    new commit

--- crypto-dashboard/script.js
+++ crypto-dashboard/script.js
@@ -1,39 +1,44 @@
-const api =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+let cryptoData = [
+  { name: "Bitcoin", current_price: 65000, change: 1.2 },
+  { name: "Ethereum", current_price: 3200, change: -0.8 },
+  { name: "Solana", current_price: 140, change: 2.5 },
+  { name: "Cardano", current_price: 0.45, change: -1.1 },
+];
 
-let previousData = [];
-
-async function fetchCrypto() {
-  const res = await fetch(api);
-  const data = await res.json();
-
+function renderCrypto() {
   const tbody = document.getElementById("crypto");
   tbody.innerHTML = "";
 
-  data.forEach((coin, i) => {
-    let prev = previousData[i];
-
-    let changeClass = "";
-    if (coin.price_change_percentage_24h > 0) {
-      changeClass = "up";
-    } else {
-      changeClass = "down";
-    }
+  cryptoData.forEach((coin) => {
+    const changeClass = coin.change > 0 ? "up" : "down";
 
     tbody.innerHTML += `
       <tr>
         <td>${coin.name}</td>
-        <td>$${coin.current_price}</td>
+        <td>$${coin.current_price.toFixed(2)}</td>
         <td class="${changeClass}">
-          ${coin.price_change_percentage_24h.toFixed(2)}%
+          ${coin.change.toFixed(2)}%
         </td>
       </tr>
     `;
   });
-
-  previousData = data;
 }
 
-fetchCrypto();
+function updatePrices() {
+  cryptoData = cryptoData.map((coin) => {
+    const randomChange = (Math.random() * 4 - 2).toFixed(2);
 
-setInterval(fetchCrypto, 60000);
+    const newPrice = coin.current_price * (1 + randomChange / 100);
+
+    return {
+      ...coin,
+      current_price: newPrice,
+      change: Number(randomChange),
+    };
+  });
+
+  renderCrypto();
+}
+
+renderCrypto();
+setInterval(updatePrices, 2000);
